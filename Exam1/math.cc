41ea73eeb2fb53b9166ce880d6ba80d7ec41eaae
18r441m <18r441m@gmail.com>
2020-02-12 20:31:01 -0500

    Exam Review Answer

--- Exam1/math.cc
+++ Exam1/math.cc
@@ -8,16 +8,40 @@ using std::cout;
 using std::endl;
 
 int main(int argc, char** argv) {
+	if (argc < 2) {
+		cout << "Invalid number of inputs" << endl;
+		return 1;
+	}
+
 	// run the conditionals on the arg
-	cout << argv[1] << endl;
-	if (argv[1] == "power")
+	string arg1 = argv[1];
+
+
+	if (arg1 == "power") {
+		if (argc < 4) {
+			cout << "Invalid number of inputs" << endl;
+			return 1;
+		}
+		string arg2 = argv[2];
+		string arg3 = argv[3];
 		cout << power(std::stod(argv[2]), std::stod(argv[3])) << endl;
-	else if (argv[1] == "factorial" && argv[2] == "ans")
-		cout << factorialans(std::stoi(argv[3])) << endl;
+	}
 	
-	else if (argv[1] == "factorial" && argv[2] == "list")
-		cout << factoriallist(std::stoi(argv[3])) << endl;
+	else if (arg1 == "factorial") {
+		if (argc < 4) {
+			cout << "Invalid number of inputs" << endl;
+			return 1;
+		}
+		string arg2 = argv[2];
+		string arg3 = argv[3];
+		if (arg2 == "ans")
+			cout << factorialans(std::stoi(arg3)) << endl;
+		else if (arg2 == "list")
+			cout << factoriallist(std::stoi(arg3)) << endl;
+	}
+
 	else
 		cout << "Invalid input" << endl;
+	
 	return 0;	
 }
